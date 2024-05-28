b240555fdfa0a97a43b9eabd52867fc6931c3fb7
kdtTetz <xenosign@naver.com>
2024-05-28 22:38:35 +0900

    240528 수업 준비

---
+++ vue2/src/components/CheckboItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li><input type="checkbox" v-model="checked" />옵션</li>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxItem',
+  data() {
+    return {
+      checked: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
